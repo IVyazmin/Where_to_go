2f543fa7d4296ec94b7bcc24687334acdaf1c126
iljavyazmin <ilja.vyazmin@mail.ru>
2023-04-11 21:11:58 +0300

    add inline preview

--- places/admin.py
+++ places/admin.py
@@ -1,16 +1,26 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import Place, Image
 
 admin.site.register(Image)
 
 
-class ImageInline(admin.StackedInline):
+class ImageInline(admin.TabularInline):
     model = Image
+    fields = ["image", "get_image", "number"]
+    readonly_fields = ["get_image"]
+    extra = 1
+
+    def get_image(self, obj):
+        if hasattr(obj.image, "url"):
+            return format_html('<img src="{url}" style="max-height:200px;"/>'.format(
+                url=obj.image.url
+            ))
+        return ""
 
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
-    inlines = [
-        ImageInline,
-    ]
+    inlines = [ImageInline]
+
